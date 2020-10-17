08e59b7cc0e8c1e2f9128d72f424da3cc2acac81
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-17 16:47:09 +0100

    Loosely Coupled

---
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -0,0 +1,12 @@
+import implemantation.Mentor;
+import service.FullTimeMentor;
+import service.MentorAccount;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+
+        FullTimeMentor fullTime = new FullTimeMentor();
+        MentorAccount mentor = new MentorAccount(fullTime);
+        mentor.manageAccount();
+    }
+}
