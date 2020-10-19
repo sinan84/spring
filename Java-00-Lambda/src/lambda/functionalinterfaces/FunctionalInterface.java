9f8650215a9b2a24e49d3b40b8677974e251def7
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-19 21:02:10 +0100

    Lambda

---
+++ Java-00-Lambda/src/lambda/functionalinterfaces/FunctionalInterface.java
@@ -0,0 +1,31 @@
+package lambda.functionalinterfaces;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class FunctionalInterface {
+    public static void main(String[] args) {
+
+        //Accepts a single parameter and returns a single value
+        Function<Integer,Double> half = a -> a/2.0;
+        System.out.println(half.apply(10));
+
+        //Does not accept any arguments and returns an object
+        Supplier<Double> randomValue = () -> Math.random();
+        System.out.println(randomValue.get());
+
+        //Accepts a single argument and does not return any result
+        Consumer<Integer> display = a -> System.out.println(a);
+        display.accept(10);
+
+        //Accepts a single argument and returns true or false
+        Predicate<Integer> lesserThan = i -> (i<10);
+        System.out.println(lesserThan.test(5));
+
+        // Commit
+
+
+    }
+}
