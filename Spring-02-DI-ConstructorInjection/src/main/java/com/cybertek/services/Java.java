3e058423716c54d6ee6b31744dfc5e67946bbe64
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-21 00:51:41 +0100

    DI-ConstructorInjection

--- Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
@@ -1,16 +1,23 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
 
 public class Java implements Course {
-    public Java(OfficeHours officeHours) {
-        this.officeHours = officeHours;
-    }
+//    OfficeHours officeHours;
+//
+//    public Java(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
 
-    OfficeHours officeHours;
+    ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
 
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours :" + (20 + officeHours.getHours()));
+        System.out.println("Weekly teaching hours :" + (20 + extraSessions.getHours()));
     }
 }
