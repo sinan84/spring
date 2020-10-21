a880e388e76fe5bd5c8483ff0c6dd21bd99957fa
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-21 02:20:14 +0100

    Bean LifeCycle - Init and Destroy Methods

--- Spring-04-BeanLifeCycle-InitAndDestroyMethod/src/main/java/com/cybertek/CybertekApp.java
+++ Spring-04-BeanLifeCycle-InitAndDestroyMethod/src/main/java/com/cybertek/CybertekApp.java
@@ -1,16 +1,14 @@
 package com.cybertek;
 
-import interfaces.Course;
+import com.cybertek.interfaces.Course;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class CybertekApp {
     public static void main(String[] args) {
         ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
-        Course course1 = container.getBean("java", Course.class);
-        Course course2 = container.getBean("java", Course.class);
-        System.out.println("Pointing to the same object : " + (course1 == course2));
-        System.out.println("Memory location for the course1 :" + course1);
-        System.out.println("Memory location for the course2 :" + course2);
+        Course course = container.getBean("java", Course.class);
+        course.getTeachingHours();
+        ((ClassPathXmlApplicationContext)container).close();
     }
 }
