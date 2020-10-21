e69a5402e04c92dbc662e0890523242da8004c69
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-21 01:12:59 +0100

    DI-SetterInjection

---
+++ Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,27 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+
+public class Java implements Course {
+   private OfficeHours officeHours;
+
+    public OfficeHours getOfficeHours() {
+        return officeHours;
+    }
+
+    public void setOfficeHours(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+//    ExtraSessions extraSessions;
+//
+//    public Java(ExtraSessions extraSessions) {
+//        this.extraSessions = extraSessions;
+//    }
+
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours :" + (20 + officeHours.getHours()));
+    }
+}
