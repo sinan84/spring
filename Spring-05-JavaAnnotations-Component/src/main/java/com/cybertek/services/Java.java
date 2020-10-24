444d799435836412cc31e172ac8bc0c30138c708
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-24 15:58:07 +0100

    JavaAnnotations-Component

--- Spring-05-JavaAnnotations-Component/src/main/java/com/cybertek/services/Java.java
+++ Spring-05-JavaAnnotations-Component/src/main/java/com/cybertek/services/Java.java
@@ -1,7 +1,9 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Java implements Course {
 
     public void getTeachingHours() {
