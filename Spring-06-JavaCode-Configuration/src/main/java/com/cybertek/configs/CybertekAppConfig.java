6c2a95d7c6740c64a26df764416aebfcd980ba90
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-24 19:43:24 +0100

    JavaCode-Configuration

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/configs/CybertekAppConfig.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/configs/CybertekAppConfig.java
@@ -1,9 +1,32 @@
 package com.cybertek.configs;
 
+import com.cybertek.interfaces.ExtraSessions;
+import com.cybertek.services.Java;
+import com.cybertek.services.OfficeHours;
+import com.cybertek.services.Selenium;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.cybertek")
 public class CybertekAppConfig {
+    @Bean
+    public Java java(){
+        return new Java();
+    }
+    @Bean
+    public Selenium selenium(){
+        return new Selenium(extraSessions());
+    }
+
+//    @Bean
+//    public OfficeHours officeHours(){
+//        return new OfficeHours();
+//    }
+
+    @Bean
+    public ExtraSessions extraSessions(){
+        return new OfficeHours();
+    }
 }
--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Selenium.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Selenium.java
@@ -1,12 +1,18 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
 
 public class Selenium implements Course {
+    private ExtraSessions extraSessions;
+
+    public Selenium(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
 
     public void getTeachingHours() {
 
-        System.out.println("Weekly teaching hours : 20");
+        System.out.println("Weekly teaching hours : " + (20 + extraSessions.getHours()));
 
     }
 }
