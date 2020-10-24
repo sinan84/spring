2dbbac6acfaf3cbafce8ec0945a4f8e51cb565ae
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-24 16:55:02 +0100

    JavaAnnotations-Qualifier

--- Spring-05-JavaAnnotations-Qualifier/src/main/java/cybertek/services/Java.java
+++ Spring-05-JavaAnnotations-Qualifier/src/main/java/cybertek/services/Java.java
@@ -2,12 +2,18 @@ package cybertek.services;
 
 import cybertek.interfaces.Course;
 import cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
 
     private ExtraSessions extraSessions;
+
+    @Autowired
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
 
     public void getTeachingHours() {
 
