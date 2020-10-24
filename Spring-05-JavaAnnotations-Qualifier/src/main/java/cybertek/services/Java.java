6fd8475bff107fea914f3f6d1b371d3cc04f8134
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-24 16:53:03 +0100

    JavaAnnotations-Qualifier

--- Spring-05-JavaAnnotations-Qualifier/src/main/java/cybertek/services/Java.java
+++ Spring-05-JavaAnnotations-Qualifier/src/main/java/cybertek/services/Java.java
@@ -1,14 +1,17 @@
 package cybertek.services;
 
 import cybertek.interfaces.Course;
+import cybertek.interfaces.ExtraSessions;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
 
+    private ExtraSessions extraSessions;
+
     public void getTeachingHours() {
 
-            System.out.println("Weekly teaching hours : 30" );
+            System.out.println("Weekly teaching hours :" + (extraSessions.getHours()) );
 
     }
 }
--- Spring-05-JavaAnnotations-Qualifier/src/main/java/cybertek/services/MockInterviewHours.java
+++ Spring-05-JavaAnnotations-Qualifier/src/main/java/cybertek/services/MockInterviewHours.java
@@ -1,10 +1,12 @@
 package cybertek.services;
 
 import cybertek.interfaces.ExtraSessions;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MockInterviewHours implements ExtraSessions {
     @Override
     public int getHours() {
-        return 0;
+        return 9;
     }
 }
