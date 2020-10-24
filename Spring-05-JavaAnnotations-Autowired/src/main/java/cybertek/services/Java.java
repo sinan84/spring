5be8f4523cd6570025c2ccf6ea486ca6a9902e9b
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-24 16:30:26 +0100

    JavaAnnotations-Autowired

--- Spring-05-JavaAnnotations-Autowired/src/main/java/cybertek/services/Java.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/cybertek/services/Java.java
@@ -1,12 +1,18 @@
 package cybertek.services;
 
 import cybertek.interfaces.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
 
     private OfficeHours officeHours;
+
+    @Autowired
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
 
     public void getTeachingHours() {
 
