5b27a5aa17710b5654af7d8c9425800d166b527d
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-24 16:34:35 +0100

    JavaAnnotations-Autowired

--- Spring-05-JavaAnnotations-Autowired/src/main/java/cybertek/services/Java.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/cybertek/services/Java.java
@@ -9,7 +9,7 @@ public class Java implements Course {
 
     private OfficeHours officeHours;
 
-    @Autowired
+    //@Autowired -->if we have only one cons we dont need to put this annotations
     public Java(OfficeHours officeHours) {
         this.officeHours = officeHours;
     }
--- Spring-05-JavaAnnotations-Autowired/src/main/java/cybertek/services/Selenium.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/cybertek/services/Selenium.java
@@ -1,12 +1,18 @@
 package cybertek.services;
 
 import cybertek.interfaces.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Selenium implements Course {
 
     private OfficeHours officeHours;
+
+    @Autowired
+    public Selenium(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
 
     public void getTeachingHours() {
 
