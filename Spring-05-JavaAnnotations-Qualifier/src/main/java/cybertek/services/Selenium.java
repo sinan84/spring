9bf8d6207c35a0258decb52929b382ed73ed73b7
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-24 16:47:12 +0100

    JavaAnnotations-Qualifier

---
+++ Spring-05-JavaAnnotations-Qualifier/src/main/java/cybertek/services/Selenium.java
@@ -0,0 +1,13 @@
+package cybertek.services;
+
+import cybertek.interfaces.Course;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Selenium implements Course {
+    public void getTeachingHours() {
+
+        System.out.println("Weekly teaching hours : 10");
+
+    }
+}
