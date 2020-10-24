972bb02af6069c1b6629fe8728bd9aae9d5f836f
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-24 16:36:16 +0100

    JavaAnnotations-Autowired

--- Spring-05-JavaAnnotations-Autowired/src/main/java/cybertek/services/Java.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/cybertek/services/Java.java
@@ -10,7 +10,17 @@ public class Java implements Course {
     private OfficeHours officeHours;
 
     //@Autowired -->if we have only one cons we dont need to put this annotations
-    public Java(OfficeHours officeHours) {
+//    public Java(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+
+
+    public OfficeHours getOfficeHours() {
+        return officeHours;
+    }
+
+    @Autowired
+    public void setOfficeHours(OfficeHours officeHours) {
         this.officeHours = officeHours;
     }
 
