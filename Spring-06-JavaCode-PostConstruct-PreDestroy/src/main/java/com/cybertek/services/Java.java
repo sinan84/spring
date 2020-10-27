a8fe81a56c0cd63635f98d2846a6860ee718dfda
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-27 23:21:14 +0000

    Post Construct and Pre Destroy Methods

---
+++ Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,25 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class Java implements Course {
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours : 10" );
+    }
+
+    @PostConstruct
+    public void postConstruct(){
+        System.out.println("Executing postConstruct method");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("Executing preDestroy method");
+    }
+
+}
