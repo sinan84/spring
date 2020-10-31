d2973abef79960af5190c4c75d9d0f0e5e284a44
ilker000 <ilkerdagdemir91@gmail.com>
2020-10-31 18:34:19 +0000

    Carpet Cost Calculator

---
+++ Spring-07-Carper-Cost-Calculator/src/main/java/com/spring/services/carpet/CarperVA.java
@@ -0,0 +1,30 @@
+package com.spring.services.carpet;
+
+import com.spring.enums.City;
+import com.spring.interfaces.carpetPrices.Carpet;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Component
+public class CarperVA implements Carpet {
+
+    private static final Map<City,BigDecimal> squarePriceForCity = new HashMap<>();
+    static {
+        squarePriceForCity.put(City.MCLEAN, new BigDecimal("4.32"));
+        squarePriceForCity.put(City.ARLINGTON, new BigDecimal("2.92"));
+        squarePriceForCity.put(City.FAIRFAX, new BigDecimal("5.34"));
+
+    }
+
+    @Override
+    public BigDecimal getStfTPrice(City city) {
+
+        Optional<Map.Entry<City, BigDecimal>> collect = squarePriceForCity.entrySet().stream().filter(x -> x.getKey() == city).findFirst();
+
+        return null;
+    }
+}
---
+++ Spring-07-Carper-Cost-Calculator/src/main/java/com/spring/services/carpet/CarpetTX.java
@@ -0,0 +1,4 @@
+package com.spring.services.carpet;
+
+public class CarpetTX {
+}
