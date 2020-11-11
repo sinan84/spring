df69af147dbdd42bc25a705cd5fe9e4aaf884e5d
ilker000 <ilkerdagdemir91@gmail.com>
2020-11-11 01:32:40 +0000

    MVC Controller

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/spring/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/spring/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,8 +18,18 @@ public class HomeController {
         return "home";
     }
 
-    @RequestMapping("/merry")
+    @RequestMapping(method = RequestMethod.POST, value = "/merry")
     public String getRequestMapping3(){
+        return "home";
+    }
+
+    @GetMapping("/spring")
+    public String getMappingExample(){
+        return "home";
+    }
+
+    @PostMapping("/spring")
+    public String getMappingExample2(){
         return "home";
     }
 }
