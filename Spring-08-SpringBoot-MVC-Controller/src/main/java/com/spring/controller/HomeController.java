372774d8f8086a85c67a0825e81bc6ec3c2cd36b
ilker000 <ilkerdagdemir91@gmail.com>
2020-11-11 01:03:20 +0000

    MVC Controller

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/spring/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/spring/controller/HomeController.java
@@ -1,4 +1,12 @@
 package com.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+    @RequestMapping
+    public String getRequestMapping(){
+        return "home";
+    }
 }
