3bcd53d347b1da33738524f044115aecfdc3598e
ilker000 <ilkerdagdemir91@gmail.com>
2020-11-11 01:14:36 +0000

    MVC Controller

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/spring/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/spring/controller/HomeController.java
@@ -2,11 +2,22 @@ package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
     @RequestMapping
     public String getRequestMapping(){
+        return "home";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/ilker")
+    public String getRequestMapping2(){
+        return "home";
+    }
+
+    @RequestMapping("/merry")
+    public String getRequestMapping3(){
         return "home";
     }
 }
