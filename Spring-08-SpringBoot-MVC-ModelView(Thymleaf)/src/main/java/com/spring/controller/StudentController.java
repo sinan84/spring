def201b3252c8141d6202690e8b5add67bb5169b
ilker000 <ilkerdagdemir91@gmail.com>
2020-11-14 16:33:19 +0000

    Thymeleaf

--- Spring-08-SpringBoot-MVC-ModelView(Thymleaf)/src/main/java/com/spring/controller/StudentController.java
+++ Spring-08-SpringBoot-MVC-ModelView(Thymleaf)/src/main/java/com/spring/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class StudentController {
 
     @GetMapping("/welcome")
-    public String homePage(){
+    public String homePage(Model model){
+        model.addAttribute("name", "Ilker");
+
         return "student/welcome";
     }
 }
