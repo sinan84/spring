e61782816808ddb72e109d76d964e0756fb3894b
ilker000 <ilkerdagdemir91@gmail.com>
2020-11-18 01:38:53 +0000

    Form

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/spring/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/spring/controller/MentorController.java
@@ -4,6 +4,7 @@ import com.spring.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -22,6 +23,11 @@ public class MentorController {
         model.addAttribute("batchList",batchList);
 
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String submitForm(){
+        return "mentor/mentor-confirmation";
     }
 
 }
