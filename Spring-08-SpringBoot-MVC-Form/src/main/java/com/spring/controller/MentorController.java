0bbadd666399ff7051ec3f3cddc3f277961acb3a
ilker000 <ilkerdagdemir91@gmail.com>
2020-11-18 01:33:21 +0000

    Form

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/spring/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/spring/controller/MentorController.java
@@ -18,7 +18,7 @@ public class MentorController {
 
         model.addAttribute("mentor",new Mentor());
 
-        List<String> batchList = Arrays.asList("B7","B8","B9","B10","B11","B12","E1","E2","E3","E4");
+        List<String> batchList = Arrays.asList("B1","B2","B3","B4","B5","B6","E1","E2","E3","E4");
         model.addAttribute("batchList",batchList);
 
         return "mentor/mentor-register";
