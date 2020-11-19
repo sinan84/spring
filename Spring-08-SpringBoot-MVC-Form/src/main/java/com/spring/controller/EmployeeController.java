f4c45a6a6cc4038e1a4d20ad1bfdb5adbc324b7f
ilker000 <ilkerdagdemir91@gmail.com>
2020-11-19 01:22:17 +0000

    Register Form

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/spring/controller/EmployeeController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/spring/controller/EmployeeController.java
@@ -5,8 +5,12 @@ import com.spring.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/employee")
@@ -18,7 +22,10 @@ public class EmployeeController {
         return "/employee/employee-create";
     }
     @PostMapping("/list")
-    public String employeeList(){
+    public String employeeList(@ModelAttribute("employee") Employee employee, Model model){
+        model.addAttribute("employeeList", Arrays.asList(employee));
+        int birthYear = LocalDate.parse(employee.getBirthday()).getYear();
+        model.addAttribute("age", LocalDate.now().getYear()-birthYear);
         return "employee/employee-list";
     }
 }
