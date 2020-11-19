8932f5a00f4b0e073ddb38c5e5d74aa7f2e8fd33
ilker000 <ilkerdagdemir91@gmail.com>
2020-11-19 01:04:51 +0000

    Register Form

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/spring/controller/EmployeeController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/spring/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.spring.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +16,9 @@ public class EmployeeController {
         model.addAttribute("employee", new Employee());
         model.addAttribute("stateList", DataGenerator.getStateList());
         return "/employee/employee-create";
+    }
+    @PostMapping("/list")
+    public String employeeList(){
+        return "employee/employee-list";
     }
 }
