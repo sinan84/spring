8c73f27c0d02adea7ffaa8368f75f6457e2bb35f
ilker000 <ilkerdagdemir91@gmail.com>
2020-11-19 00:24:03 +0000

    Register Form

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/spring/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+    @GetMapping("/register")
+    public String employeeCreate(){
+
+        return "/employee/employee-create";
+    }
+}
