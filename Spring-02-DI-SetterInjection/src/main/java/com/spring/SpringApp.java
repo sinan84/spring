c2c40b887f6bdbe19a93e3e25aa9098ad5bb8695
ilker000 <ilkerdagdemir91@gmail.com>
2020-11-29 22:33:27 +0000

    Dependency Injection - Setter Injection

--- Spring-02-DI-SetterInjection/src/main/java/com/spring/SpringApp.java
+++ Spring-02-DI-SetterInjection/src/main/java/com/spring/SpringApp.java
@@ -5,7 +5,7 @@ import com.spring.interfaces.Course;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class CybertekApp {
+public class SpringApp {
     public static void main(String[] args) {
         ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
         Course course = container.getBean("selenium", Course.class);
