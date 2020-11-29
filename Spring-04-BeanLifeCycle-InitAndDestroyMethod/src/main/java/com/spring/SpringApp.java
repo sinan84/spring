158a954f9d9f1db2b6253c838972282d615777c9
ilker000 <ilkerdagdemir91@gmail.com>
2020-11-29 22:36:44 +0000

    Init and Destroy Method

--- Spring-04-BeanLifeCycle-InitAndDestroyMethod/src/main/java/com/spring/SpringApp.java
+++ Spring-04-BeanLifeCycle-InitAndDestroyMethod/src/main/java/com/spring/SpringApp.java
@@ -4,7 +4,7 @@ import com.spring.interfaces.Course;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class CybertekApp {
+public class SpringApp {
     public static void main(String[] args) {
         ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
         Course course = container.getBean("java", Course.class);
