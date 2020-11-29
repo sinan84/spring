f981621fb106153e5ea3c76056690cc6cffdfcfe
ilker000 <ilkerdagdemir91@gmail.com>
2020-11-29 22:41:12 +0000

    Java Annotations - Component

--- Spring-05-JavaAnnotations-Component/src/main/java/com/spring/SpringApp.java
+++ Spring-05-JavaAnnotations-Component/src/main/java/com/spring/SpringApp.java
@@ -4,7 +4,7 @@ import com.spring.interfaces.Course;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class CybertekApp {
+public class SpringApp {
     public static void main(String[] args) {
         ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
         Course course = container.getBean("API", Course.class); //IF ALL LETTERS OF CLASSNAME ARE CAPITAL, YOU DONT NEED TO LOWERCASE THE FIRST LETTER...
