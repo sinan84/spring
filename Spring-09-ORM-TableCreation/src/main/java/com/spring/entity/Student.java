e9688b2847ade48ba2efb20780234b24fc7dcdae
ilker000 <ilkerdagdemir91@gmail.com>
2020-12-12 19:10:50 +0000

    ORM - Table Creation

--- Spring-09-ORM-TableCreation/src/main/java/com/spring/entity/Student.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/spring/entity/Student.java
@@ -1,6 +1,12 @@
 package com.spring.entity;
 
+import com.spring.enums.Gender;
+
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 
 @Entity
 @Table(name = "students")
@@ -9,12 +15,27 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "studentFirstName")
-    private String firstName;
+    private String firstName;  //first_name
     @Column(name = "studentLastName")
     private String lastName;
-    @Column(name = "studentEmailAddress")
+    @Column(name = "studentEmailAddres")
     private String email;
     @Transient
     private String city;
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+    @Temporal(TemporalType.TIME)
+    private Date birthTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date birthDateTime;
+    @Column(columnDefinition = "DATE")
+    private LocalDate localDate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime localTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime localDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 
 }
